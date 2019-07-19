20fea17662f73b972d26ce887d10ffae76b5b284
mufirdhan <fikri18ti@mahasiswa.pcr.ac.id>
2019-07-19 10:34:02 +0700

    signup.php

--- signup.php
+++ signup.php
@@ -1,19 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login form</title>
+	<title>SingUp form</title>
 	<link rel="stylesheet" type="text/css" href="loginsignup-style.css">
 <body>
 	<div class="loginbox">
 	<img src="avatar.jpg" class="avatar">
-	<h1>Login</h1>
+	<h1>Sign-Up</h1>
 	<form>
+		<p>Name</p>
+		<input type="text" name="" placeholder="Enter Your Name">
+		<p>E-mail</p>
+		<input type="text" name="" placeholder="Enter Your Email">
+		<p>Phone</p>
+		<input type="text" name="" placeholder="Enter Your Phone Number">
 		<p>Username</p>
 		<input type="text" name="" placeholder="Enter Username">
 		<p>Password</p>
 		<input type="Password" name="" placeholder="Enter Password">
-		<input type="submit" name="" value="Login">
-		<a href="http://localhost/project/Login-SignUp/signup.php">Don't have an account?</a>
+		<input type="submit" name="" value="Sign-Up">
 	</form>
 
 	</div>
