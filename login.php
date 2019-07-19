deff126c19debc13bf199babdad2a32b35bd47e5
mufirdhan <fikri18ti@mahasiswa.pcr.ac.id>
2019-07-19 10:29:22 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login form</title>
+	<link rel="stylesheet" type="text/css" href="Style.css">
+<body>
+	<div class="loginbox">
+	<img src="avatar.jpg" class="avatar">
+	<h1>Login</h1>
+	<form>
+		<p>Username</p>
+		<input type="text" name="" placeholder="Enter Username">
+		<p>Password</p>
+		<input type="Password" name="" placeholder="Enter Password">
+		<input type="submit" name="" value="Login">
+		<a href="http://localhost/project/Login-SignUp/signup.php">Don't have an account?</a>
+	</form>
+
+	</div>
+
+</body>
+</head>
+<body>
+
+</body>
+</html>
