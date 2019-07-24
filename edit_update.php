b74656ef6b8023cb42a5e1d4118a37eecef8ae29
farahdilla <farahdilla18ti@mahasiswa.pcr.ac.id>
2019-07-24 21:39:23 +0700

    Update edit_update.php

--- edit_update.php
+++ edit_update.php
@@ -3,20 +3,20 @@
 include 'koneksi.php';
  
 // menangkap data yang di kirim dari form
-$id = $_POST['id'];
+$id_login = $_POST['id_login'];
 $nama = $_POST['nama'];
 $email = $_POST['email'];
 $nohp = $_POST['nohp'];
 $username = $_POST['username'];
 $password = $_POST['password'];
  
+$update = mysqli_query($db, "UPDATE user SET nama='$nama', email='$email', nohp='$nohp', username='$username', password='$password' WHERE id_login='$id_login'");
 // update data ke database
-if (!empty($nama) && !empty($email) && !empty($nohp) && !empty($username) && !empty($password)) {
-	mysqli_query($db, "UPDATE user SET nama='$nama', email='$email', nohp='$nohp', username='$username', password='$password' WHERE id_login='$id'" );
-
-	header('location: crud.php?status=sukses');
+if ($update) {
+echo "<script>alert('Data Berhasil Diedit');document.location.href='crud.php'</script>";
+       
 }else{
-	header('location: edit.php?status=gagal');
+    echo "<script>alert('Data Gagal Diedit');document.location.href='crud.php?id_login='$id_login''</script>";
+       
 }
- 
 ?>
