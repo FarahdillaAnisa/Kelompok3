85f1befb0f0802eaed723cef92a17933aac19ba9
farahdilla <farahdilla18ti@mahasiswa.pcr.ac.id>
2019-08-05 04:54:00 +0700

    menghapus file crud lama

--- cekloginadm.php
+++ cekloginadm.php
@@ -3,10 +3,10 @@ session_start();
 $username = $_POST['username'];
 $password = $_POST['password'];
 if ($username == 'admin' && $password == 'admin') {
-	header('location: crud.php?status=sukses');
+	header('location: browse-users.php?status=sukses');
 }
 else{
-	header('location: adminlog.html?status=gagal');
+	header('location: adminlogin.html?status=gagal');
 }
 
 ?>
